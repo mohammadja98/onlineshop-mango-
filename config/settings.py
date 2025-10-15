d10d98cde28fcc25da686c5aab734302eeac8247
Your Name <you@example.com>
2025-10-15 23:29:41 +0330

    add 404.html

--- config/settings.py
+++ config/settings.py
@@ -25,13 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-DEBUG = env.bool("DJANGO_DEBUG")
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
 
 # Application definition
 
