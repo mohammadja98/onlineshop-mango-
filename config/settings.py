de6df7f4b96894e4ba3fbd44caa016ef1cc567f9
Your Name <you@example.com>
2025-10-15 18:27:21 +0330

    change fonts

--- config/settings.py
+++ config/settings.py
@@ -157,10 +157,9 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
